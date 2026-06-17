6e78056f430be6fe57d1172d2c135f9de629588f
phelipeamorimchagas <phelipeamorimchagas@gmail.com>
2026-06-17 19:28:52 -0300

    feat: Adiciona estrutura para funcao divisao

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 // #include "funcao_soma.h"
 // #include "funcao_subtracao.h"
 // #include "funcao_multiplicacao.h"
-// #include "funcao_divisao.h"
+#include "funcao_divisao.h"
 
 int main() {
     printf("Bem-vindo ao nosso projeto de calculadora!\n");
@@ -22,11 +22,11 @@ int main() {
     // int resultado_multiplicacao = multiplicar(7, 6);
     // printf("Multiplicacao (7 * 6): %d\n", resultado_multiplicacao);
 
-    // float resultado_divisao = dividir(100.0, 4.0);
-    // printf("Divisao (100.0 / 4.0): %.2f\n", resultado_divisao);
+    float resultado_divisao = dividir(100.0, 4.0);
+    printf("Divisao (100.0 / 4.0): %.2f\n", resultado_divisao);
 
-    // float resultado_divisao_erro = dividir(10.0, 0.0); // Teste de erro
-    // printf("Divisao (10.0 / 0.0): %.2f\n", resultado_divisao_erro); // Deve indicar erro
+    float resultado_divisao_erro = dividir(10.0, 0.0); // Teste de erro
+    printf("Divisao (10.0 / 0.0): %.2f\n", resultado_divisao_erro); // Deve indicar erro
 
     return 0;
 }
